bd5d9b57073f0bce71fd168d89fd14185819a225
Kostas Mavropalias <mavropalias@gmail.com>
2019-08-22 10:38:58 +0100

    it works

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const functions = require("firebase-functions");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev, conf: { distDir: "next" } });
+const handle = app.getRequestHandler();
+
+exports.next = functions.https.onRequest(async (req, res) => {
+  await app.prepare();
+  handle(req, res);
+});
