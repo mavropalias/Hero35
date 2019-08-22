b21ea76c851b0c89dca3b726a1d1dc6e8c471c87
Kostas Mavropalias <mavropalias@gmail.com>
2019-08-22 10:41:54 +0100

    Ignore ts warning

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,7 @@ const functions = require("firebase-functions");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
+// @ts-ignore
 const app = next({ dev, conf: { distDir: "next" } });
 const handle = app.getRequestHandler();
 
