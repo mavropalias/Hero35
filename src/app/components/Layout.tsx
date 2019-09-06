58f036f4abd31fb2963ecc4e1e3705145ed27dae
Kostas Mavropalias <mavropalias@gmail.com>
2019-09-07 00:53:08 +0100

    Add _app and _document for better MUI support

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -4,11 +4,10 @@ import { ThemeProvider } from "@material-ui/styles";
 import appTheme from "../appTheme";
 
 const Layout = props => (
-  <ThemeProvider theme={appTheme}>
-    <CssBaseline />
+  <>
     <Header />
     {props.children}
-  </ThemeProvider>
+  </>
 );
 
 export default Layout;
