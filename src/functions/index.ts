6c7b51bd07d4d1ce63cb6aa8b01ddae1a817a4da
Kostas Mavropalias <mavropalias@gmail.com>
2019-09-16 17:25:27 +0100

    Update deploy config

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,14 +1,18 @@
 import * as functions from "firebase-functions";
 import next from "next";
 
-const app = next({ dev: false, conf: { distDir: "dist/functions/next" } });
+const app = next({
+  dev: false,
+  dir: __dirname,
+  conf: { distDir: "next" }
+});
 const handle = app.getRequestHandler();
 
 const ssr = functions.https.onRequest(async (req, res) => {
-  // Cache for 12 hours on the client and 3 days on the server
+  // Cache for 12 hours on the client and 7 days on the server
   res.setHeader(
     "Cache-control",
-    `public, max-age=${12 * 60 * 60}, s-maxage=${3 * 24 * 60 * 60}`
+    `public, max-age=${12 * 60 * 60}, s-maxage=${7 * 24 * 60 * 60}`
   );
   await app.prepare();
   return handle(req, res);
