f9f83b1e405e3a508825d1e289b882a5295c0191
Kostas Mavropalias <mavropalias@gmail.com>
2019-09-18 19:06:49 +0100

    Add Firebase functions

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -8,6 +8,14 @@ const CACHE_CONTROL = `public, max-age=${12 * 60 * 60}, s-maxage=${7 *
   60 *
   60}`;
 
+// Allow all origins
+// TODO limit this to hero.com and admin users
+const ACCESS_CONTROL = "*";
+const API_HEADERS = {
+  "Access-Control-Allow-Origin": ACCESS_CONTROL,
+  "Cache-control": CACHE_CONTROL
+};
+
 admin.initializeApp();
 const db = admin.firestore();
 
@@ -21,13 +29,13 @@ const app = next({
 });
 const handle = app.getRequestHandler();
 const ssr = functions.https.onRequest(async (req, res) => {
-  res.setHeader("Cache-control", CACHE_CONTROL);
+  res.set("Cache-control", CACHE_CONTROL);
   await app.prepare();
   return handle(req, res);
 });
 
 /**
- * Get Event by id
+ * Get Event
  */
 const event = functions.https.onRequest(async (req, res) => {
   const eventId = req.query.id;
@@ -43,13 +51,96 @@ const event = functions.https.onRequest(async (req, res) => {
     });
     event.editions = editions;
   }
-  res.setHeader("Cache-control", CACHE_CONTROL);
+  res.set(API_HEADERS);
   res.json(event);
 });
 
+/**
+ * Get Edition
+ */
+const edition = functions.https.onRequest(async (req, res) => {
+  const eventId = req.query.eventId;
+  const editionId = req.query.editionId;
+  if (!eventId) res.send("eventId is required");
+  if (!editionId) res.send("editionId is required");
+  const docSnap = await db
+    .collection("events")
+    .doc(eventId)
+    .collection("editions")
+    .doc(editionId)
+    .get();
+  const edition = docSnap.data();
+  res.set(API_HEADERS);
+  res.json(edition);
+});
+
+/**
+ * Get recent Edition
+ */
+const recentEditions = functions.https.onRequest(async (req, res) => {
+  const docSnap = await db
+    .collectionGroup("editions")
+    .where("status", "==", "published")
+    .orderBy("endDate", "desc")
+    .limit(6)
+    .get();
+  let editions = [];
+  docSnap.forEach(doc => {
+    editions.push(doc.data());
+  });
+  res.set(API_HEADERS);
+  res.json(editions);
+});
+
+/**
+ * Get Talk
+ */
+const talk = functions.https.onRequest(async (req, res) => {
+  const eventId = req.query.eventId;
+  const editionId = req.query.editionId;
+  const talkId = req.query.talkId;
+  if (!eventId) res.send("eventId is required");
+  if (!editionId) res.send("editionId is required");
+  if (!talkId) res.send("talkId is required");
+  const docSnap = await db
+    .collection("events")
+    .doc(eventId)
+    .collection("editions")
+    .doc(editionId)
+    .collection("talks")
+    .doc(talkId)
+    .get();
+  const talk = docSnap.data();
+  res.set(API_HEADERS);
+  res.json(talk);
+});
+
+/**
+ * Get recent Talks
+ */
+const recentTalks = functions.https.onRequest(async (req, res) => {
+  const docSnap = await db
+    .collectionGroup("talks")
+    .where("type", "==", "2")
+    .orderBy("date", "desc")
+    .orderBy("order", "desc")
+    .limit(6)
+    .get();
+  let talks = [];
+  docSnap.forEach(doc => {
+    talks.push(doc.data());
+  });
+  res.set(API_HEADERS);
+  res.json(talks);
+});
+
 const heroes = {
+  edition,
   event,
-  ssr
+  recentEditions,
+  ssr,
+  talk,
+  recentTalks
 };
 
 export { heroes };
