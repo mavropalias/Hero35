4a9866175fce9990ffa4a5bd909ab448db8b0162
Kostas Mavropalias <mavropalias@gmail.com>
2019-09-18 07:48:07 +0100

    Disable gzip compression in Next

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   poweredByHeader: false,
   distDir: "../../dist/functions/next",
+  compress: false,
   env: {
     STORAGE_PATH:
       "https://firebasestorage.googleapis.com/v0/b/heroes-9c313.appspot.com/o/"
