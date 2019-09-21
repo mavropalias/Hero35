1c8ca2181c4ed503c911cf00d7a799c5a7bbe6f3
Kostas Mavropalias <mavropalias@gmail.com>
2019-09-21 19:09:12 +0100

    Clean-up routes

--- src/app/constants/routes.ts
+++ src/app/constants/routes.ts
@@ -1,27 +1,7 @@
 const HOME = `/`;
-const USER_HOME = `/home/`;
-const SIGN_UP = `/signup/`;
-const SIGN_IN = `/signin/`;
-const ACCOUNT = `/account/`;
-const EDIT_EVENT = `/edit-event/`;
-const PASSWORD_FORGET = `/pw-forget/`;
-const EDITION = `/event/:eventid/:editionid`;
-const TALK = `/event/:eventid/:editionid/:talk`;
-const EVENT = `/event/:id`;
-const EVENTS = `/events/:sort?`;
-const EVENTS_HOME = `/events/new`;
+const ACCOUNT = `/account`;
 
 export default {
   HOME,
-  USER_HOME,
-  SIGN_UP,
-  SIGN_IN,
-  ACCOUNT,
-  EDIT_EVENT,
-  PASSWORD_FORGET,
-  EDITION,
-  TALK,
-  EVENT,
-  EVENTS_HOME,
-  EVENTS
+  ACCOUNT
 };
