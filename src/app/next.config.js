7459ac48706b90060682b34e97ef9c0f2be396ff
Kostas Mavropalias <mavropalias@gmail.com>
2019-10-04 07:23:47 +0100

    Add bundle analysis

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -1,11 +1,29 @@
 const withCSS = require("@zeit/next-css");
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
 
-module.exports = withCSS({
-  poweredByHeader: false,
-  distDir: "../../dist/functions/next",
-  compress: false,
-  env: {
-    STORAGE_PATH:
-      "https://firebasestorage.googleapis.com/v0/b/heroes-9c313.appspot.com/o/"
-  }
-});
+module.exports = withBundleAnalyzer(
+  withCSS({
+    analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: "static",
+        reportFilename: "../../../../bundles/server.html"
+      },
+      browser: {
+        analyzerMode: "static",
+        reportFilename: "../../../../bundles/client.html"
+      }
+    },
+    webpack(config) {
+      return config;
+    },
+    poweredByHeader: false,
+    distDir: "../../dist/functions/next",
+    compress: false,
+    env: {
+      STORAGE_PATH:
+        "https://firebasestorage.googleapis.com/v0/b/heroes-9c313.appspot.com/o/"
+    }
+  })
+);
