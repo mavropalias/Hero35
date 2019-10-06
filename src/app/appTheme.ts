0370a7e8a26923467a09e99c90b82db34bea6bff
Kostas Mavropalias <mavropalias@gmail.com>
2019-10-06 17:35:29 +0100

    Set primary theme color to blue

--- src/app/appTheme.ts
+++ src/app/appTheme.ts
@@ -1,9 +1,11 @@
 import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+import { blue } from "@material-ui/core/colors";
 
 const theme = responsiveFontSizes(
   createMuiTheme({
     palette: {
       type: "dark",
+      primary: blue,
       background: {
         paper: "#171717",
         default: "#121212"
