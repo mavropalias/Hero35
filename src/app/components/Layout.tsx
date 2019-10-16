4b95010d61c2e3aca889b026b03399adfe08a297
Kostas Mavropalias <mavropalias@gmail.com>
2019-10-16 19:06:41 +0100

    Update social media image

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -30,16 +30,16 @@ const Layout = ({
         <meta
           data-react-helmet="true"
           name="twitter-image"
-          content="https://hero35.com/static/favicons/apple-icon-180x180.png"
+          content="https://hero35.com/static/social-media-square.png"
         />
         <meta name="og:image:type" content="image/jpeg" />
         <meta
           name="og:image"
-          content="https://hero35.com/static/favicons/apple-icon-180x180.png"
+          content="https://hero35.com/static/social-media-square.png"
         />
         <meta
           name="og:image:secure_url"
-          content="https://hero35.com/static/favicons/apple-icon-180x180.png"
+          content="https://hero35.com/static/social-media-square.png"
         />
       </Head>
       <Header />
