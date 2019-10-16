b56bcd9f45e8b18fb4011f4d5e2c8cd0ee22cd71
Kostas Mavropalias <mavropalias@gmail.com>
2019-10-16 18:44:56 +0100

    Add social media meta tags

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -20,6 +20,27 @@ const Layout = ({
         <title>{headTitle}</title>
         <meta name="description" content={description} />
         <meta name="keywords" content={`React,ReactJS,${keywords}`} />
+        <meta property="og:title" content={headTitle} />
+        <meta property="og:description" content={description} />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@hero35official" />
+        <meta name="twitter:title" content={headTitle} />
+        <meta name="twitter:description" content={description} />
+        <meta
+          data-react-helmet="true"
+          name="twitter-image"
+          content="https://hero35.com/static/favicons/apple-icon-180x180.png"
+        />
+        <meta name="og:image:type" content="image/jpeg" />
+        <meta
+          name="og:image"
+          content="https://hero35.com/static/favicons/apple-icon-180x180.png"
+        />
+        <meta
+          name="og:image:secure_url"
+          content="https://hero35.com/static/favicons/apple-icon-180x180.png"
+        />
       </Head>
       <Header />
       {children}
