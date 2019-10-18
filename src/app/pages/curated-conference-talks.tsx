e7237035a113700b64b92b8a89f5d6ae6f7844b4
Kostas Mavropalias <mavropalias@gmail.com>
2019-10-18 19:57:32 +0100

    Tidy up curated talks page

--- src/app/pages/curated-conference-talks.tsx
+++ src/app/pages/curated-conference-talks.tsx
@@ -1,11 +1,13 @@
 import Layout from "../components/Layout";
+import theme from "../appTheme";
 import {
   makeStyles,
   createStyles,
   Theme,
   Typography,
   Container,
-  Grid
+  Grid,
+  Paper
 } from "@material-ui/core";
 import { Talk } from "../schema";
 import Database from "../services/Database";
@@ -15,7 +17,10 @@ import CuratedTalk from "../components/CuratedTalk";
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
-      marginTop: theme.spacing(2)
+      paddingTop: theme.spacing(8)
+    },
+    paper: {
+      marginBottom: theme.spacing(4)
     }
   })
 );
@@ -27,24 +32,36 @@ interface Props {
 const CuratedPage: NextPage<Props> = ({ talks }) => {
   const classes = useStyles({});
 
+  let style = {
+    background: `linear-gradient(35deg, ${theme.palette.background.paper} 0%, #444444 100%)`
+  };
+
   return (
     <Layout
       title="Curated React conference talks"
       description="Must-watch React talks from developer conferences around the world, hand-picked by our editorial team."
     >
-      <Container className={classes.container}>
-        <Typography variant="h5" component="h1">
-          Curated React talks
-        </Typography>
-        <Typography variant="subtitle1">
-          Must-watch React talks from developer conferences around the world,
-          hand-picked by our editorial team.
-        </Typography>
-        <Typography variant="body2" color="textSecondary" paragraph>
-          The ingredients of a great conference talk, are: fascinating content
-          and/or high educational value, captivating delivery, and clear audio
-          quality.
-        </Typography>
+      <Paper className={classes.paper} style={style} square>
+        <Container className={classes.container}>
+          <Grid container spacing={1}>
+            <Grid item xs={12} md={8}>
+              <Typography variant="h1">Curated React talks</Typography>
+            </Grid>
+            <Grid item xs={12} md={8}>
+              <Typography variant="body1">
+                Must-watch React talks from conferences around the world,
+                hand-picked by our editorial team.
+              </Typography>
+              <Typography variant="caption" color="textSecondary" paragraph>
+                The ingredients of a great conference talk, are: fascinating
+                content and/or high educational value, captivating delivery, and
+                clear audio quality.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Container>
+      </Paper>
+      <Container>
         <Grid container spacing={4}>
           {talks.map(talk => (
             <Grid key={talk.id} item xs={12} sm={6} md={4}>
