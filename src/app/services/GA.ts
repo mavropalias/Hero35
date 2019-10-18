95366d0f3cbbb10387f5f4f80062a6a3a051e45e
Kostas Mavropalias <mavropalias@gmail.com>
2019-10-18 19:55:37 +0100

    Add null check

--- src/app/services/GA.ts
+++ src/app/services/GA.ts
@@ -4,16 +4,20 @@ export const GA_TRACKING_ID = "UA-749229-33";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = url => {
-  window.gtag("config", GA_TRACKING_ID, {
-    page_path: url
-  });
+  if (window.gtag) {
+    window.gtag("config", GA_TRACKING_ID, {
+      page_path: url
+    });
+  }
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }) => {
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value: value
-  });
+  if (window.gtag) {
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value: value
+    });
+  }
 };
