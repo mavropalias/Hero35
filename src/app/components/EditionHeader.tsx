1f5050f2ecbf0a8ead467b821bc42ffaaa0f9dca
Kostas Mavropalias <mavropalias@gmail.com>
2019-11-26 18:03:00 +0000

    Add highlights video in Edition page & update header UI

---
+++ src/app/components/EditionHeader.tsx
@@ -0,0 +1,313 @@
+import { default as NextLink } from "next/link";
+import YouTube from "react-youtube";
+import { EventEdition } from "../schema";
+import {
+  makeStyles,
+  Theme,
+  createStyles,
+  Typography,
+  Button,
+  Box,
+  Link,
+  Divider,
+  Chip,
+  Paper
+} from "@material-ui/core";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import {
+  OpenInNew as LinkIcon,
+  LocalPlay as DistinctiveIcon,
+  Payment as TicketIcon
+} from "@material-ui/icons";
+import CATEGORIES from "../constants/categories";
+import { useContext, useState } from "react";
+import { StackContext } from "./context-providers/StackContextProvider";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    chip: {
+      marginRight: theme.spacing(1),
+      marginBottom: theme.spacing(0.5),
+      marginTop: theme.spacing(0.5),
+      "&:first-child": {
+        marginLeft: theme.spacing(1)
+      }
+    },
+    description: {
+      whiteSpace: "pre-line"
+    },
+    distinctiveContainer: {
+      zIndex: 2,
+      position: "absolute",
+      top: theme.spacing(2),
+      color: theme.palette.secondary.main,
+      width: "100%"
+    },
+    externalLinkIcon: {
+      fontSize: theme.typography.fontSize
+    },
+    highlightsContainer: {
+      position: "relative",
+      paddingBottom: "56.25%" /* maintain 16:9 aspect ratio */,
+      paddingTop: 25,
+      height: 0,
+      pointerEvents: "none"
+    },
+    logo: {
+      maxWidth: "50%",
+      height: theme.spacing(16)
+    },
+    logoContainer: {
+      position: "absolute",
+      zIndex: 2,
+      bottom: theme.spacing(-2),
+      width: "100%",
+      textAlign: "center"
+    },
+    shadow: {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      background: `linear-gradient(0deg, ${fade(
+        theme.palette.background.default,
+        1
+      )} 11%, ${fade(theme.palette.background.default, 0)} 30%, ${fade(
+        theme.palette.background.default,
+        0
+      )} 65%, ${fade(theme.palette.background.default, 1)} 85%)`,
+      zIndex: 1
+    },
+    youtubePlayer: {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%"
+    }
+  })
+);
+
+const EditionHeader = ({ edition }: { edition: EventEdition }) => {
+  const classes = useStyles({});
+  const showTickets = (): boolean => {
+    if (!edition.ticketsUrl) return false;
+    if (edition.status !== "published-notalks") return false;
+    const currentDate = new Date();
+    const editionDate = new Date(edition.startDate);
+    if (editionDate.getTime() > currentDate.getTime()) return true;
+    else return false;
+  };
+  const shortDate = (date: string) => {
+    const startDate = new Date(date);
+    var options = {
+      month: "short",
+      day: "numeric"
+    };
+    return startDate.toLocaleDateString(undefined, options);
+  };
+  return (
+    <header>
+      <EditionHighlights edition={edition} />
+      <Box marginTop={2} textAlign="center">
+        <Typography variant="h2" component="h1" gutterBottom>
+          {edition.eventTitle} {edition.title}
+        </Typography>
+        <Typography variant="subtitle1">
+          {shortDate(edition.startDate)}&nbsp;&ndash;&nbsp;
+          {shortDate(edition.endDate)} | {edition.state || edition.city},{" "}
+          {edition.country}
+        </Typography>
+        <NextLink href="/[eventid]" as={`/${edition.eventId}`} passHref>
+          <Link color="primary">View all {edition.eventTitle} events</Link>
+        </NextLink>
+        {showTickets() && (
+          <Box m={4}>
+            <Button
+              variant="contained"
+              color="secondary"
+              href={edition.ticketsUrl}
+              target="_blank"
+              rel="noopener"
+              startIcon={<TicketIcon />}
+            >
+              Buy tickets
+            </Button>
+          </Box>
+        )}
+      </Box>
+      <Box m={4}>
+        <Divider variant="middle" />
+      </Box>
+      <EditionContentDetails edition={edition} />
+      <Box m={4}>
+        <Divider variant="middle" />
+      </Box>
+      <Typography variant="body1" className={classes.description}>
+        {edition.description}
+      </Typography>
+      <p>
+        <Link
+          href={edition.website}
+          color="textSecondary"
+          target="_blank"
+          variant="body2"
+        >
+          Official website <LinkIcon className={classes.externalLinkIcon} />
+        </Link>
+      </p>
+    </header>
+  );
+};
+
+const EditionDistinctive = () => {
+  const classes = useStyles({});
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      className={classes.distinctiveContainer}
+    >
+      <DistinctiveIcon />
+      &nbsp;
+      <Typography
+        variant="subtitle1"
+        title="This event has superior content and excellent audio / video quality. The Editor's Choice talks are only the beginning. We're confident that you'll enjoy most of the content."
+      >
+        Distinctive event
+      </Typography>
+    </Box>
+  );
+};
+
+const EditionHighlights = ({ edition }: { edition: EventEdition }) => {
+  const classes = useStyles({});
+  const [error, setError] = useState(false);
+  const highlightVideos = () => {
+    // Find highlight videos (type '9'), starting with the shortest one
+    // We need at least two items in the return array, so that YouTube can loop
+    // through them.
+    if (!edition.talks) return [];
+    let highlightVideos = edition.talks
+      .filter(talk => talk.type === "9")
+      .sort((a, b) => (a.times.totalMins > b.times.totalMins ? 1 : -1))
+      .map(talk => talk.youtubeId);
+    if (highlightVideos.length === 0) {
+      highlightVideos = [
+        edition.talks[0].youtubeId,
+        edition.talks[1].youtubeId
+      ];
+    } else if (highlightVideos.length === 1) {
+      highlightVideos.push(highlightVideos[0]);
+    }
+    return highlightVideos;
+  };
+  const youtubeOptions = {
+    height: "100%",
+    width: "100%",
+    playerVars: {
+      modestbranding: true,
+      playsinline: true,
+      rel: 0,
+      autoplay: 1,
+      mute: 1,
+      loop: 1,
+      controls: 0,
+      disablekb: 1,
+      playlist: highlightVideos()
+        .slice(1)
+        .join(",")
+    }
+  };
+  const onYoutubeError = event => {
+    setError(true);
+  };
+  return (
+    <section className={classes.highlightsContainer}>
+      <div className={classes.shadow}></div>
+      {edition.isDistinctive && <EditionDistinctive />}
+      {edition.talks && !error && (
+        <YouTube
+          className={classes.youtubePlayer}
+          videoId={highlightVideos()[0]}
+          opts={youtubeOptions}
+          onError={onYoutubeError}
+        />
+      )}
+      <div className={classes.logoContainer}>
+        <img
+          className={classes.logo}
+          src={`${process.env.STORAGE_PATH}${encodeURIComponent(
+            edition.logo
+          )}?alt=media`}
+          alt="Event logo"
+        />
+      </div>
+    </section>
+  );
+};
+
+const EditionContentDetails = ({ edition }: { edition: EventEdition }) => {
+  const { state: stateStack } = useContext(StackContext);
+  const classes = useStyles({});
+
+  return (
+    <Typography variant="body1" align="center">
+      {edition.durationMinutes > 0 ? (
+        <>
+          {edition.eventTitle} {edition.title} has{" "}
+          {(edition.durationMinutes / 60).toFixed(0)} hours of content in&nbsp;
+        </>
+      ) : (
+        <>Event category:&nbsp;&nbsp;</>
+      )}
+      {edition.categories
+        .map(cat => (
+          <span key={cat}>
+            {CATEGORIES.find(category => cat === category.id) &&
+              CATEGORIES.find(category => cat === category.id).title}
+          </span>
+        ))
+        .reduce((previous, current) => (
+          <>
+            {previous}, {current}
+          </>
+        ))}
+      {edition.tags && (
+        <>
+          <span>&nbsp;&nbsp;</span>
+          {edition.tags.map(
+            (tag, index) =>
+              tag.count > 1 && (
+                <NextLink
+                  key={index}
+                  passHref
+                  href={`/topic/[topicid]${
+                    stateStack.slug ? `?stack=${stateStack.slug}` : ""
+                  }`}
+                  as={`/topic/${tag.label.toLowerCase()}${
+                    stateStack.slug ? `?stack=${stateStack.slug}` : ""
+                  }`}
+                >
+                  <Chip
+                    component="a"
+                    color="primary"
+                    variant="outlined"
+                    size="small"
+                    key={index}
+                    label={tag.label}
+                    className={classes.chip}
+                  />
+                </NextLink>
+              )
+          )}
+        </>
+      )}{" "}
+      {edition.durationMinutes > 0 && <>and more.</>}
+    </Typography>
+  );
+};
+
+export default EditionHeader;
--- src/app/pages/[eventid]/[editionid].tsx
+++ src/app/pages/[eventid]/[editionid].tsx
@@ -1,4 +1,3 @@
-import { default as NextLink } from "next/link";
 import Layout from "../../components/Layout";
 import EditionTalks from "../../components/EditionTalks";
 import {
@@ -6,64 +5,19 @@ import {
   createStyles,
   Theme,
   Typography,
-  Link,
-  Chip,
   Container,
-  Grid,
-  Button,
-  Box,
-  Paper
+  Grid
 } from "@material-ui/core";
-import {
-  OpenInNew as LinkIcon,
-  Stars as DistinctiveIcon,
-  Payment as TicketIcon
-} from "@material-ui/icons";
 import { EventEdition } from "../../schema";
 import Database from "../../services/Database";
 import { NextPage, NextPageContext } from "next";
 import Breadcrumbs from "../../components/Breadcrumbs";
-import CATEGORIES from "../../constants/categories";
-import { useContext } from "react";
-import { StackContext } from "../../components/context-providers/StackContextProvider";
+import EditionHeader from "../../components/EditionHeader";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
       marginTop: theme.spacing(2)
-    },
-    logo: {
-      maxWidth: "50%",
-      height: theme.typography.fontSize * 4
-    },
-    chip: {
-      marginRight: theme.spacing(1),
-      marginBottom: theme.spacing(0.5),
-      marginTop: theme.spacing(0.5),
-      "&:first-child": {
-        marginLeft: theme.spacing(1)
-      }
-    },
-    chipCategory: {
-      marginRight: theme.spacing(1),
-      marginBottom: theme.spacing(0.5),
-      marginTop: theme.spacing(0.5)
-    },
-    description: {
-      whiteSpace: "pre-line"
-    },
-    distinctiveContainer: {
-      width: "100%",
-      background: `linear-gradient(35deg, ${theme.palette.secondary.dark} 0%, ${theme.palette.secondary.light} 100%)`,
-      padding: theme.spacing(2),
-      margin: theme.spacing(2, 0),
-      color: theme.palette.background.default
-    },
-    externalLinkIcon: {
-      fontSize: theme.typography.fontSize
-    },
-    icon: {
-      marginLeft: theme.spacing(1)
     }
   })
 );
@@ -73,26 +27,7 @@ interface Props {
 }
 
 const EditionDetails: NextPage<Props> = ({ edition }) => {
-  const { state: stateStack } = useContext(StackContext);
   const classes = useStyles({});
-
-  const showTickets = (): boolean => {
-    if (!edition.ticketsUrl) return false;
-    if (edition.status !== "published-notalks") return false;
-    const currentDate = new Date();
-    const editionDate = new Date(edition.startDate);
-    if (editionDate.getTime() > currentDate.getTime()) return true;
-    else return false;
-  };
-
-  const shortDate = (date: string) => {
-    const startDate = new Date(date);
-    var options = {
-      month: "short",
-      day: "numeric"
-    };
-    return startDate.toLocaleDateString(undefined, options);
-  };
 
   const breadcrumbs = [
     {
@@ -117,142 +52,7 @@ const EditionDetails: NextPage<Props> = ({ edition }) => {
       <Container className={classes.container}>
         <Grid container spacing={4}>
           <Grid item xs={12} md={6}>
-            <Grid container spacing={4}>
-              <Grid item xs={12}>
-                <img
-                  className={classes.logo}
-                  src={`${process.env.STORAGE_PATH}${encodeURIComponent(
-                    edition.logo
-                  )}?alt=media`}
-                  alt="Event logo"
-                />
-                <Typography variant="h1" component="h1" paragraph>
-                  {edition.eventTitle} {edition.title}
-                </Typography>
-                <NextLink href="/[eventid]" as={`/${edition.eventId}`} passHref>
-                  <Button color="primary">
-                    View all {edition.eventTitle} events
-                  </Button>
-                </NextLink>
-              </Grid>
-              <Grid item xs={12}>
-                <Box
-                  display="flex"
-                  alignItems="center"
-                  marginTop={1}
-                  flexWrap="wrap"
-                >
-                  {edition.durationMinutes > 0 ? (
-                    <Typography variant="body1">
-                      {(edition.durationMinutes / 60).toFixed(0)} hours of
-                      content in&nbsp;
-                    </Typography>
-                  ) : (
-                    <>Event category:&nbsp;&nbsp;</>
-                  )}
-                  {edition.categories
-                    .map(cat => (
-                      <span key={cat}>
-                        {CATEGORIES.find(category => cat === category.id) &&
-                          CATEGORIES.find(category => cat === category.id)
-                            .title}
-                      </span>
-                    ))
-                    .reduce((previous, current) => (
-                      <>
-                        {previous}, {current}
-                      </>
-                    ))}
-                  {edition.tags && (
-                    <>
-                      <span>&nbsp;&nbsp;</span>
-                      {edition.tags.map(
-                        (tag, index) =>
-                          tag.count > 1 && (
-                            <NextLink
-                              key={index}
-                              passHref
-                              href={`/topic/[topicid]${
-                                stateStack.slug
-                                  ? `?stack=${stateStack.slug}`
-                                  : ""
-                              }`}
-                              as={`/topic/${tag.label.toLowerCase()}${
-                                stateStack.slug
-                                  ? `?stack=${stateStack.slug}`
-                                  : ""
-                              }`}
-                            >
-                              <Chip
-                                component="a"
-                                color="primary"
-                                variant="outlined"
-                                size="small"
-                                key={index}
-                                label={tag.label}
-                                className={classes.chip}
-                              />
-                            </NextLink>
-                          )
-                      )}
-                    </>
-                  )}{" "}
-                  {edition.durationMinutes > 0 && <>and more.</>}
-                </Box>
-              </Grid>
-              {showTickets() && (
-                <Grid item xs={12}>
-                  <Button
-                    variant="contained"
-                    color="secondary"
-                    href={edition.ticketsUrl}
-                    target="_blank"
-                    rel="noopener"
-                  >
-                    Buy tickets
-                    <TicketIcon className={classes.icon} />
-                  </Button>
-                </Grid>
-              )}
-              {edition.isDistinctive && (
-                <Paper className={classes.distinctiveContainer}>
-                  <Box display="flex" alignItems="center">
-                    <DistinctiveIcon />
-                    &nbsp;
-                    <Typography variant="h6">Distinctive event</Typography>
-                  </Box>
-                  <Typography variant="body2">
-                    This event has superior content and excellent audio / video
-                    quality. The Editor's Choice talks are only the beginning.
-                    We're confident that you'll enjoy most of the content.
-                  </Typography>
-                </Paper>
-              )}
-              <Grid item xs={12}>
-                <Typography variant="h5" component="h2">
-                  Event info
-                </Typography>
-                <Typography variant="overline">
-                  {shortDate(edition.startDate)}&ndash;
-                  {shortDate(edition.endDate)} | {edition.state || edition.city}
-                  , {edition.country}
-                </Typography>
-                <Typography variant="body1" className={classes.description}>
-                  {edition.description}
-                </Typography>
-                <p>
-                  <Link
-                    href={edition.website}
-                    color="textSecondary"
-                    target="_blank"
-                    variant="body2"
-                  >
-                    Official website{" "}
-                    <LinkIcon className={classes.externalLinkIcon} />
-                  </Link>
-                </p>
-              </Grid>
-            </Grid>
+            <EditionHeader edition={edition} />
           </Grid>
           <Grid item xs={12} md={6}>
             {edition.talks && edition.talks.length > 0 ? (
@@ -267,19 +67,6 @@ const EditionDetails: NextPage<Props> = ({ edition }) => {
       </Container>
     </Layout>
   );
-};
-
-// TODO implement this
-const EditionHighlights = ({ edition }: { edition: EventEdition }) => {
-  const iframe = `<iframe
-                    width="100%"
-                    height="400"
-                    src="https://www.youtube-nocookie.com/embed/7S_7fKLgjXU?autoplay=1&mute=1&loop=1&controls=0&disablekb=1&modestbranding=1"
-                    frameborder="0"
-                    allow="autoplay;encrypted-media"
-                  />`;
-
-  return <div dangerouslySetInnerHTML={{ __html: iframe }} />;
 };
 
 interface QueryProps {
